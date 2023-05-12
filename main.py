68f2fd7c1585f90de9f43a6be6eb778090c79231
pavithrakumar1118 <84068975+pavithrakumar1118@users.noreply.github.com>
2023-05-12 21:25:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+from datetime import datetime
+
+MY_LAT = 12.971599
+MY_LONG = 77.594566
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+#
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "Formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+
+
+
